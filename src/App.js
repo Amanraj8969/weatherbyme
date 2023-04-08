821a38780f117b3241dcebc33f739c13e314f047
Aman kumar <araj6205444@gmail.com>
2023-04-08 22:51:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import CurrentLocation from "./currentLocation";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="container">
+        <CurrentLocation />
+      </div>
+      <div className="footer-info">
+        <a href="https://www.htmlhints.com/article/how-to-create-toggle-switch/93">
+          Download Source Code
+        </a>{" "}
+        | Developed by{" "}
+        <a target="_blank" href="https://thunderous-sherbet-58bd29.netlify.app">
+          Aman Kumar
+        </a>{" "}
+        | Powered by{" "}
+        <a target="_blank" href="https://www.goole.com/">
+          Google
+        </a>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
